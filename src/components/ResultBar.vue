78ee4a0ccf10b395d7dd0365a62f0892b1ac0547
Serap Pala <serap68040@gmail.com>
2022-07-15 14:36:54 +0300

    todo app completed

---
+++ src/components/ResultBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <!-- <small class="mt-2 d-flex justify-content-end green"> {{ todoList.length }} adet todo vardır</small> -->
+  <small class="mt-2 d-flex justify-content-end green">There is {{ itemCount }}  todo</small>
+</template>
+<script>
+export default {
+  props: ["itemCount"]
+};
+</script>
