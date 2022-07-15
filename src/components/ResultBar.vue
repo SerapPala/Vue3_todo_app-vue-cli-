aacda74ca34ed8b7c60f8a174fbdea5e81c88ff1
Serap Pala <serap68040@gmail.com>
2022-07-15 14:41:54 +0300

    todo app completed

--- src/components/ResultBar.vue
+++ src/components/ResultBar.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- <small class="mt-2 d-flex justify-content-end green"> {{ todoList.length }} adet todo vardır</small> -->
-  <small class="mt-2 d-flex justify-content-end green">There is {{ itemCount }}  todo</small>
+  <small class="mt-2 d-flex justify-content-end green">There is {{ itemCount }}  todo item</small>
 </template>
 <script>
 export default {
--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -1,7 +1,7 @@
 <template>
   <li class="d-flex justify-content-between align-items-center">
     <span>{{ item.text }}</span>
-    <button @click="deleteItem(item)" class="sm red">Sil</button>
+    <button @click="deleteItem(item)" class="sm red">Remove</button>
   </li>
 </template>
 <script>
